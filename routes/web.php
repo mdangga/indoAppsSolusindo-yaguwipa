f3487378c50121a792a39e6e960586452bd974fb
YudhaStacy <ketutyuda1322@gmail.com>
2025-06-28 09:21:03 +0800

    berita

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,19 @@
 use App\Http\Controllers\BeritaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BeritaController::class, 'beranda'])->name('beranda');
+
 Route::get('/berita/create', function () {
     return view('testing');
 });
+
 Route::get('/berita', [BeritaController::class, 'index']);
 Route::get('/berita/{slug}', [BeritaController::class, 'show']);
 Route::post('/berita', [BeritaController::class, 'store']);
 Route::put('/berita/{id}', [BeritaController::class, 'update']);
 Route::delete('/berita/{id}', [BeritaController::class, 'destroy']);
+
+Route::get('/berita', [BeritaController::class, 'index'])->name('berandaberita');
 
 Route::get('/signin', function () {
     return view('signin');
