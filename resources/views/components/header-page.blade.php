9733d9547ed41135d18d37034002b75559f66b49
YudhaStacy <ketutyuda1322@gmail.com>
2025-07-02 10:46:44 +0700

    menerapkan komponen

--- resources/views/components/header-page.blade.php
+++ resources/views/components/header-page.blade.php
@@ -1,8 +1,7 @@
-@props(['title', 'description' => '', 'id' => ''])
+@props(['title', 'description' => '', 'id' => null])
 
 <div class="sm:flex-row pb-10 gap-4 text-center">
-    <h1 @if ($id) id="{{ $id }}" @endif
-        class="mt-15 text-3xl font-semibold text-gray-900">
+    <h1 {{ $id ? "id=$id" : '' }} class="mt-15 text-3xl font-semibold text-gray-900">
         {{ $title }}
     </h1>
     @if ($description)
