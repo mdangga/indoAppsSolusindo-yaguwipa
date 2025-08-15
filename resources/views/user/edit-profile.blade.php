c1be4308fb0ec80415d5ebcc0571c62e61faaf5a
mdangga <anggamurdika2005@gmail.com>
2025-08-15 19:19:30 +0800

    update regex no telpon dan merapikan pagination pada notifikasi dan aktivitas

--- resources/views/user/edit-profile.blade.php
+++ resources/views/user/edit-profile.blade.php
@@ -145,9 +145,12 @@
                                 </div>
 
                                 <div>
-                                    <label for="no_tlp" class="block text-sm font-medium text-gray-700 mb-2">Nomor
-                                        Telepon</label>
+                                    <label for="no_tlp" class="text-sm font-medium text-gray-700">
+                                        Nomor Telepon <span class="text-gray-500 text-xs">(08..., +628...,
+                                            628...)</span>
+                                    </label>
                                     <input type="tel" id="no_tlp" name="no_tlp"
+                                        placeholder="08123456789, 628123456789, +628123456789"
                                         value="{{ old('no_tlp', $user->no_tlp ?? '') }}"
                                         class="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
                                     @error('no_tlp')
